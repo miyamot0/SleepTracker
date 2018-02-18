f2fc3c9af614029d436071d0d5646d8c3f4b4516
Shawn Gilroy <shawn.gilroy@temple.edu>
2018-02-18 10:00:32 +0000

    todos

--- SleepTracker/Views/RootHomePage.cs
+++ SleepTracker/Views/RootHomePage.cs
@@ -20,6 +20,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
+TODO:
+
+Android theme issues
+Android icon in bar/hamburger menu
+Android dismiss menu
+
 */
 
 using System.Threading.Tasks;
